c143fee9438b3e6cd04989c6cf23a182532b8f45
iniubongisemin <eugeneinie@gmail.com>
2026-09-04 17:49:30 +0100

    Started SQL course

--- intro.sql
+++ intro.sql
@@ -9,3 +9,11 @@ FROM books;
 -- Select all fields from the books table
 SELECT *
 FROM books;
+
+-- Select unique authors from the books table
+SELECT DISTINCT author
+FROM books;
+
+-- Select unique authors and genre combinations from the books table
+SELECT DISTINCT author, genre
+FROM books;
