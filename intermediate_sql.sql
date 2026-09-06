c521c1fd118591a7b7dd9d67a4599b7ece381635
iniubongisemin <eugeneinie@gmail.com>
2026-09-06 23:55:06 +0100

    FEAT: Count functionality Implemented

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -1,2 +1,15 @@
 SELECT COUNT(film_id) AS count_film_id
 FROM reviews;
+
+-- PRACTICE WITH COUNT
+-- Count the number of records in the people table
+SELECT COUNT(*) AS count_records
+FROM people;
+
+-- Count the number of birthdates in the people table
+SELECT COUNT(birthdate) AS count_birthdate
+FROM people;
+
+-- Count the records for languages and countries represented in the films table
+SELECT COUNT(language) AS count_languages, COUNT(country) AS count_countries
+FROM films;
